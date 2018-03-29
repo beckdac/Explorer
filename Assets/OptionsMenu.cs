37e74ec1fc044c17968c26c944694d9011348a7b
Dave Beck <beck.dac@live.com>
2018-03-29 16:30:41 -0700

    Initial commit from prototype sources

---
+++ Assets/OptionsMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionsMenu : MonoBehaviour {
+
+    public void MainMenu()
+    {
+        Debug.Log("MainMenu");
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
